8605d0ba99694c7b9ab93a3a8f63827fc106bdae
naren8919 <clouddevops44@gmail.com>
2025-12-22 23:13:49 +0530

    Delete all files from terraform-without-module

--- user-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y tomcat
-systemctl start tomcat
-systemctl enable tomcat
-echo "Hello from Terraform" > /var/lib/tomcat/webapps/ROOT/index.html
-
-aws s3 cp /var/log/messages s3://${bucket_name}/$(hostname)-messages.log
